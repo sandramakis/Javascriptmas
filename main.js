6681fcd32277fa3e8499c4d77da7a8aa21887e66
Sandra Nwakeze <sandramakis.dev@gmail.com>
2023-12-21 01:02:39 +0100

    ADD: Day 20- Save Santa

--- main.js
+++ main.js
@@ -550,3 +550,103 @@ const addNewNice = () => {
 sortBtn.addEventListener("click", sortLists, { once: true });
 document.querySelector("#add-naughty").addEventListener("click", addNewNaughty);
 document.querySelector("#add-nice").addEventListener("click", addNewNice);
+
+// DAY 18- Debug Jingle Words
+import JSConfetti from "js-confetti";
+const word = "santa";
+const wordArr = word.split("");
+const wordDisplay = document.getElementById("word-display");
+document.addEventListener("submit", handleGuess);
+
+function renderSpaces() {
+  const wordHtml = wordArr.map(() => {
+    return `<span class="letter">-</span>`;
+  });
+  wordDisplay.innerHTML = wordHtml.join("");
+}
+renderSpaces();
+
+function renderGuess(arr) {
+  const wordHtml = arr.map((letter) => {
+    return `<span class="letter">${letter}</span>`;
+  });
+  wordDisplay.innerHTML = wordHtml.join("");
+}
+
+function handleGuess(e) {
+  e.preventDefault();
+  /**
+   * Debug Challenge:
+   * 1. There are loads of problems with the
+   *    code below. Find them and fix them!
+   **/
+
+  /* bugs begin 🦠*/
+  let currentState = [];
+  let input = document.getElementById("users-input");
+  let guess = input.id;
+  const guessArr = guess.split(" ");
+  wordArr.foreach((letter) => {
+    if (letter === guessArr[""]) {
+      currentState.push(letter);
+    } else {
+      currentState.push(letter);
+    }
+  });
+  /* bugs end 🦠*/
+  renderGuess(currentState);
+  checkWin(guess);
+  input.value = "";
+}
+
+function checkWin(guess) {
+  if (word === guess) {
+    const jsConfetti = new JSConfetti();
+    jsConfetti.addConfetti({
+      emojis: ["🧑‍🎄", "🎅"],
+      emojiSize: 50,
+      confettiNumber: 60,
+      confettiRadius: 6,
+    });
+    jsConfetti.addConfetti();
+  }
+}
+
+// Day 20: Save Santa
+const dangerArray = [
+  ["🎅", "👺"],
+  [
+    ["🎅", "🦁"],
+    ["👹", "🎅"],
+  ],
+  [
+    [
+      ["🎅", "🐻"],
+      ["🧌", "🎅"],
+    ],
+    [
+      ["🐯", "🎅"],
+      ["🎅", "😈"],
+    ],
+  ],
+];
+
+function saveSanta(arr) {
+  // Your code here
+
+  const flattenArr = arr.flat(Infinity);
+  const regex =
+    /[\u{1F47A}\u{1F981}\u{1F42F}\u{1F608}\u{1F43B}\u{1F9D8}\u{1F479}\u{1F479}]/iu;
+
+  const filteredEmojis = flattenArr.filter((emoji) => !regex.test(emoji));
+
+  document.querySelector(
+    "section.day-20-section p.initial"
+  ).textContent = `Initial Array: [ ${flattenArr}]`;
+  document.querySelector(
+    "section.day-20-section p.current"
+  ).textContent = `Better:  ${filteredEmojis}`;
+}
+
+saveSanta(dangerArray);
+// Check the returned results from saveSanta()
